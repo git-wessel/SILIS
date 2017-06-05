95cf19e9cb8fad6fa104007c071b73ceed1d7180
Wesley <wesley@wesleykuilema.nl>
2017-06-05 15:26:59 +0200

    The Beginning

---
+++ login.php
@@ -0,0 +1,75 @@
+<?php 
+    require("common.php"); 
+     
+    $submitted_username = '';   
+    if(!empty($_POST)) 
+    {          
+        $query = " 
+            SELECT 
+                id, 
+                username, 
+                password, 
+                salt, 
+                email 
+            FROM users 
+            WHERE 
+                username = :username 
+        ";          
+        $query_params = array( 
+            ':username' => $_POST['username'] 
+        ); 
+         
+        try 
+        {  
+            $stmt = $db->prepare($query); 
+            $result = $stmt->execute($query_params); 
+        } 
+        catch(PDOException $ex) 
+        { 
+            die("Failed to run query: " . $ex->getMessage()); 
+        }
+        $login_ok = false; 
+        $row = $stmt->fetch(); 
+        if($row) 
+        { 
+            $check_password = hash('sha256', $_POST['password'] . $row['salt']); 
+            for($round = 0; $round < 65536; $round++) 
+            { 
+                $check_password = hash('sha256', $check_password . $row['salt']); 
+            } 
+             
+            if($check_password === $row['password']) 
+            { 
+                // If they do, then we flip this to true 
+                $login_ok = true; 
+            } 
+        } 
+        if($login_ok) 
+        { 
+             
+            unset($row['salt']); 
+            unset($row['password']); 
+             
+            $_SESSION['user'] = $row; 
+             
+            header("Location: private.php"); 
+            die("Redirecting to: private.php"); 
+        } 
+        else 
+        { 
+            print("Login Failed."); 
+            $submitted_username = htmlentities($_POST['username'], ENT_QUOTES, 'UTF-8'); 
+        } 
+    } 
+?> 
+<h1>Login</h1> 
+<form action="login.php" method="post"> 
+    Username:<br /> 
+    <input type="text" name="username" value="<?php echo $submitted_username; ?>" /> 
+    <br /><br /> 
+    Password:<br /> 
+    <input type="password" name="password" value="" /> 
+    <br /><br /> 
+    <input type="submit" value="Login" /> 
+</form> 
+<a href="register.php">Register</a>
